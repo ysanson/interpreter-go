9f10458df5f8d4f906dfcc2e478d970bb38dadf3
Yvan SANSON <sansonyvan@gmail.com>
2024-11-04 21:15:11 +0900

    Added infix expression parsing

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -127,3 +127,21 @@ func (pe *PrefixExpression) String() string {
 	return out.String()
 }
 
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteRune('(')
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteRune(')')
+	return out.String()
+}
